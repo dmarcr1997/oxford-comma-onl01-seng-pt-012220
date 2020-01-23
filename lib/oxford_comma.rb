91ee417a62807c78eb2250d86d2ff7159a6696e7
Damon R <dmarcr1997@gmail.com>
2020-01-23 21:50:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-  if array.len
-  last_elm = array.pop()
-  array.join(", ")  
-  array << last_elm
+  if array.count() > 2
+    last_elm = array.pop()
+    array.join(", ")  
+    array << last_elm
+  else 
+    array.join("and ")
 end
